b089cd6841fc1affeedbe1a949e86888c0895c30
Enrique Serrano <e.serranogar@gmail.com>
2025-07-28 15:09:13 +0200

    update main app

--- src/main_app.py
+++ src/main_app.py
@@ -4,7 +4,7 @@ import joblib
 import os
 
 from feature_engineering import predict_sarimax, feature_engineering_random_forest
-from statsmodels.tsa.arima.model import ARIMA
+from statsmodels.tsa.arima.model import ARIMAResults
 from statsmodels.tsa.statespace.sarimax import SARIMAXResults
 
 # Load all models and encoders
@@ -29,7 +29,7 @@ if all([os.path.exists(LR_MODEL_PATH),
         os.path.exists(RF_MODEL_PATH)]):
     linear_regresion_model = joblib.load(LR_MODEL_PATH)
     # arima_model = joblib.load(ARIMA_MODEL_PATH)
-    arima_model = ARIMA.load(ARIMA_MODEL_PATH)
+    arima_model = ARIMAResults.load(ARIMA_MODEL_PATH)
     ses_model = joblib.load(SES_MODEL_PATH)
     # sarimax_model = joblib.load(SARIMAX_MODEL_PATH)
     sarimax_model = SARIMAXResults.load(SARIMAX_MODEL_PATH)
