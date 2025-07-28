35b19776bcbd8c51f5ffa514f461e1d61b552a8d
Enrique Serrano <e.serranogar@gmail.com>
2025-07-28 12:53:54 +0200

    updating deployment

--- src/main_app.py
+++ src/main_app.py
@@ -1,52 +1,102 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
 import joblib
 import os
-from feature_engineering import feature_engineering_prediction
 
-# Load the trained model
-MODEL_PATH = "models/best_model.pkl"
-SUPPLIER_AVG_COST_PATH = "models/supplier_avg_cost.joblib"
-SUPPLY_AVG_COST_PATH = "models/supply_ref_avg_cost.joblib"
-if os.path.exists(MODEL_PATH):
-    model = joblib.load(MODEL_PATH)
-    supplier_avg_cost = joblib.load(SUPPLIER_AVG_COST_PATH)
-    supply_ref_avg_cost = joblib.load(SUPPLY_AVG_COST_PATH)
+from feature_engineering import predict_sarimax, feature_engineering_random_forest
+
+# Load all models and encoders
+LR_MODEL_PATH = "models/linear_regresion_model.pkl"
+ARIMA_MODEL_PATH = "models/arima_model.pkl"
+SES_MODEL_PATH = "models/ses_model.pkl"
+SARIMAX_MODEL_PATH = "models/sarimax_model.pkl"
+RF_MODEL_PATH = "models/random_forest_model.pkl"
+
+ENCODERS_SARIMAX_PATH = "models/label_encoders_sarimax.pkl"
+SCALER_SARIMAX_PATH = "models/scaler_sarimax.pkl"
+META_SARIMAX_PATH = "models/sarimax_meta.pkl"
+
+ENCODERS_RANDOM_FOREST_PATH = "models/label_encoders_random_forest.pkl"
+
+DF_MONTHLY_PATH = "models/df_monthly.pkl"
+
+if all([os.path.exists(LR_MODEL_PATH),
+        os.path.exists(ARIMA_MODEL_PATH),
+        os.path.exists(SES_MODEL_PATH),
+        os.path.exists(SARIMAX_MODEL_PATH),
+        os.path.exists(RF_MODEL_PATH)]):
+    linear_regresion_model = joblib.load(LR_MODEL_PATH)
+    arima_model = joblib.load(ARIMA_MODEL_PATH)
+    ses_model = joblib.load(SES_MODEL_PATH)
+    sarimax_model = joblib.load(SARIMAX_MODEL_PATH)
+    random_forest_model = joblib.load(RF_MODEL_PATH)
+
+    encoders_sarimax = joblib.load(ENCODERS_SARIMAX_PATH)
+    scaler_sarimax = joblib.load(SCALER_SARIMAX_PATH)
+    meta_sarimax = joblib.load(META_SARIMAX_PATH)
+
+    encoders_random_forest = joblib.load(ENCODERS_RANDOM_FOREST_PATH)
+
+    df_monthly = joblib.load(DF_MONTHLY_PATH)
 else:
     st.error("Model file not found. Please train the model and place it in the 'models' directory.")
     st.stop()
 
 st.title("Industrial Goods Cost Forecasting")
-st.write("This application predicts the cost of industrial goods based on historical data.")
+st.write("This application predicts the cost of industrial goods based on historical data, in particular for the supply reference 'TA 1/2\"NPT-M 316/316L'.")
 
 # Sample supplier and supply lists (replace with actual data source)
-supplier_list = ["ThyssenKrupp Materials Ibérica", "Valbruna Ibérica, S.L.", "Empresa Santa Lucía, S.A."]
-supply_list = ["BRE 130 AISI-316/316L", 'BWN 2"150RF S40 C', 'PL 1"300X3MM C']
+supplier_list = ["DELCORTE", "SIDSA", "EGARENSE"]
 
 # User inputs
 st.sidebar.header("Input Features")
 order_date = st.sidebar.date_input("Order Date")
 supplier_name = st.sidebar.selectbox("Supplier Name", supplier_list)
-supply_reference = st.sidebar.selectbox("Supply Reference", supply_list)
 delivery_date = st.sidebar.date_input("Delivery Date")
-quantity = st.sidebar.number_input("Quantity", min_value=1, value=10)
+quantity = st.sidebar.number_input("Quantity")
 
 # Create input DataFrame
 input_data = pd.DataFrame({
     "order_date": [order_date],
     "supplier_name": [supplier_name],
-    "supply_reference": [supply_reference],
     "delivery_date": [delivery_date],
-    "quantity": [quantity],
-    "price_change_rate": [np.nan]
+    "quantity": [quantity]
 })
 
-# Preprocess input
-processed_input = feature_engineering_prediction(input_data, supplier_avg_cost, supply_ref_avg_cost)
-x_processed_values = processed_input[['quantity', 'price_change_rate', 'supplier_encoded', 'supply_ref_encoded', 'lead_time', 'month', 'year']]
+X_rf, _, _, _ = feature_engineering_random_forest(input_data, label_encoders=encoders_random_forest, min_date=pd.Timestamp("2020-01-01"))
 
 # Make prediction
 if st.button("Predict Cost"):
-    prediction = model.predict(x_processed_values)[0]
-    st.success(f"Predicted Cost: €{prediction:.2f}")
+    target_date = pd.Timestamp(input_data.iloc[0]["order_date"])
+    
+    # Linear Regression Prediction
+    date_ordinal = [[target_date.toordinal()]]
+    pred_lr = linear_regresion_model.predict(date_ordinal)[0]
+
+    # ARIMA and SES Prediction
+    steps = (target_date - df_monthly.index[-1]).days // 30
+
+    if steps > 0:
+        pred_ses = ses_model.forecast(steps).iloc[-1]
+        pred_arima = arima_model.forecast(steps).iloc[-1]
+    else:
+        # If date is in historical data
+        if target_date in df_monthly.index:
+            pred_ses = df_monthly.loc[target_date, "unit_value"]
+            pred_arima = df_monthly.loc[target_date, "unit_value"]
+        else:
+            # If date is before historical data, use first available value
+            pred_ses = df_monthly.iloc[0]["unit_value"]
+            pred_arima = df_monthly.iloc[0]["unit_value"]
+    
+    # SARIMAX Prediction
+    prediction_sarimax = predict_sarimax(input_data, sarimax_model, encoders_sarimax, scaler_sarimax, meta_sarimax)
+
+    # Random Forest Prediction
+    prediction_rf = random_forest_model.predict(X_rf)[0]
+
+    st.success(f"Predicted Cost (LINEAR REGRESSION): {pred_lr:.2f} €")
+    st.success(f"Predicted Cost (ARIMA): {pred_arima:.2f} €")
+    st.success(f"Predicted Cost (SES): {pred_ses:.2f} €")
+    st.success(f"Predicted Cost (SARIMAX): {prediction_sarimax['prediccion']:.2f} €")
+    st.success(f"Predicted Cost (RANDOM FOREST): {prediction_rf:.2f} €")
